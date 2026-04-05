eb60cfcb6af7d32684c68481ac5273d5b729d4b3
Johanna <johanna.jimenez12@gmail.com>
2026-04-05 10:31:24 -0300

    Actualizo

--- src/main/java/com/uade/tpo/demo/entity/Orden.java
+++ src/main/java/com/uade/tpo/demo/entity/Orden.java
@@ -1,5 +1,35 @@
 package com.uade.tpo.demo.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
 public class Orden {
-    
+
+    public Orden() {
+    }
+
+    public Orden(Long idUsuario, String fechaVenta, Float total, String estado, Long idCarrito, String metodoPago) {
+        this.idUsuario = idUsuario;
+        this.fechaVenta = fechaVenta;
+        this.total = total;
+        this.estado = estado;
+        this.idCarrito = idCarrito;
+        this.metodoPago = metodoPago;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idOrden;
+
+    private Long idUsuario;
+    private String fechaVenta;
+    private Float total;
+    private String estado;
+    private Long idCarrito;
+    private String metodoPago;
 }
