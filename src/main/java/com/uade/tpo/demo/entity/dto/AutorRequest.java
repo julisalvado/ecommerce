7777d7766622ec27fc2116a3f836a43306068386
FionaPardo <110065793+FionaPardo@users.noreply.github.com>
2026-04-12 12:25:30 -0300

    Add AutorRequest DTO class with fields

---
+++ src/main/java/com/uade/tpo/demo/entity/dto/AutorRequest.java
@@ -0,0 +1,10 @@
+package com.uade.tpo.demo.entity.dto;
+
+import lombok.Data;
+
+@Data
+public class AutorRequest {
+    private String nombre;
+    private String apellido;
+    private String nacionalidad;
+}
