0d705f8b845cd9ade6dc2ac622a7f75ef5dfae0d
FionaPardo <110065793+FionaPardo@users.noreply.github.com>
2026-04-12 13:09:46 -0300

    Create LibroService interface for book management

---
+++ src/main/java/com/uade/tpo/demo/service/LibroService.java
@@ -0,0 +1,22 @@
+package com.uade.tpo.demo.service;
+
+import java.util.List;
+
+import com.uade.tpo.demo.entity.Libro;
+import com.uade.tpo.demo.entity.dto.LibroRequest;
+import com.uade.tpo.demo.exceptions.RecursoNotFoundException;
+
+public interface LibroService {
+
+    List<Libro> getLibros();
+
+    Libro getLibroById(Long id) throws RecursoNotFoundException;
+
+    Libro createLibro(LibroRequest request) throws RecursoNotFoundException;
+
+    List<Libro> getLibrosByGenero(Long idGenero) throws RecursoNotFoundException;
+
+    List<Libro> getLibrosByPrecio(float precioMin, float precioMax);
+
+    List<Libro> getLibrosByAutor(Long idAutor) throws RecursoNotFoundException;
+}
